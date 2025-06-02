951f823841626c86cc90a48f7f7920c4b1ca61b5
Artur Gomes <156813822+arog-bioinfo@users.noreply.github.com>
2025-06-02 23:19:08 +0100

    .

--- comm_sim_scripts/smetana_script.bash
+++ comm_sim_scripts/smetana_script.bash
@@ -5,13 +5,13 @@ conda activate smetana
 
 cd /home/arog/models/xml 
 
-#Default
-smetana ic_1306.xml iECSF_1327.xml iNRG857_1313.xml iYL1228.xml iEcSMS35_1347.xml iSDY_1059.xml iEC1356_Bl21DE3.xml iECUMN_1333.xml iUMNK88_1353.xml iECSE_1348.xml iETEC_1333.xml iUTI89_1310.xml -o /home/arog/Documents/GitHub/HGMB_Project/comm_results/smetana_all
+#Default (resultado tem alguma aleatoriadade natural. Ver algoritmo)
+smetana ic_1306.xml iCN900.xml iECSF_1327.xml iNRG857_1313.xml iYL1228.xml iEcSMS35_1347.xml iSDY_1059.xml iEC1356_Bl21DE3.xml iECUMN_1333.xml iUMNK88_1353.xml iECSE_1348.xml iETEC_1333.xml iUTI89_1310.xml -o /home/arog/Documents/GitHub/HGMB_Project/comm_results/smetana_all
 
-#Detailed
-smetana ic_1306.xml iECSF_1327.xml iNRG857_1313.xml iYL1228.xml iEcSMS35_1347.xml iSDY_1059.xml iEC1356_Bl21DE3.xml iECUMN_1333.xml iUMNK88_1353.xml iECSE_1348.xml iETEC_1333.xml iUTI89_1310.xml --detailed -o /home/arog/Documents/GitHub/HGMB_Project/comm_results/smetana_all
+#Detailed (resultado tem alguma aleatoriadade natural. Ver algoritmo)
+smetana ic_1306.xml iCN900.xml iECSF_1327.xml iNRG857_1313.xml iYL1228.xml iEcSMS35_1347.xml iSDY_1059.xml iEC1356_Bl21DE3.xml iECUMN_1333.xml iUMNK88_1353.xml iECSE_1348.xml iETEC_1333.xml iUTI89_1310.xml --detailed -o /home/arog/Documents/GitHub/HGMB_Project/comm_results/smetana_all
 
-#Detailed and Medium Composition
-smetana ic_1306.xml iECSF_1327.xml iNRG857_1313.xml iYL1228.xml iEcSMS35_1347.xml iSDY_1059.xml iEC1356_Bl21DE3.xml iECUMN_1333.xml iUMNK88_1353.xml iECSE_1348.xml iETEC_1333.xml iUTI89_1310.xml --detailed --molweight -o /home/arog/Documents/GitHub/HGMB_Project/comm_results/smetana_all_medium
+#Detailed and Medium Composition (resultado tem alguma aleatoriadade natural. Ver algoritmo)
+smetana ic_1306.xml iCN900.xml iECSF_1327.xml iNRG857_1313.xml iYL1228.xml iEcSMS35_1347.xml iSDY_1059.xml iEC1356_Bl21DE3.xml iECUMN_1333.xml iUMNK88_1353.xml iECSE_1348.xml iETEC_1333.xml iUTI89_1310.xml --detailed --molweight -o /home/arog/Documents/GitHub/HGMB_Project/comm_results/smetana_all_medium
 
 conda deactivate
